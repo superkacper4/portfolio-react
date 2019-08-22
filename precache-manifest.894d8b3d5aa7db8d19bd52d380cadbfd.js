2ca634ff6326c31b170c4157156bdd5104ed94bd
Kacper Kuczewski <kucza06@wp.pl>
2019-08-22 18:29:03 +0200

    Updates

---
+++ precache-manifest.894d8b3d5aa7db8d19bd52d380cadbfd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a02e728a830bafc92e38b6501f3ae73c",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "2e921295b0d68a9de48c",
+    "url": "/portfolio-react/static/js/2.6e1ec8d3.chunk.js"
+  },
+  {
+    "revision": "d28d80fd03ae83e9acb2",
+    "url": "/portfolio-react/static/js/main.bd6996eb.chunk.js"
+  },
+  {
+    "revision": "d62b9e40651021ec9466",
+    "url": "/portfolio-react/static/js/runtime~main.7fd600c8.js"
+  }
+]);
